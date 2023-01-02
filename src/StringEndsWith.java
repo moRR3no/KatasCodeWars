fe7d9a207e1dfa806c162e0f57dd381d8473b220
Konrad <konradpstrongowski@gmail.com>
2023-01-02 16:17:23 +0100

    StringEndsWith commit

---
+++ src/StringEndsWith.java
@@ -0,0 +1,17 @@
+/**
+ * Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument
+ * (also a string).
+ *
+ * Examples:
+ *
+ * solution('abc', 'bc') // returns true
+ * solution('abc', 'd') // returns false
+ */
+
+
+//Level of difficulty: 7kyu
+public class StringEndsWith {
+    public static boolean solution (String str, String ending) {
+        return str.endsWith(ending);
+    }
+}
